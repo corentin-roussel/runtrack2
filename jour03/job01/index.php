5696e0cdf95ace356f86086239ea21607821bd70
corentin-roussel <corentin.roussel@laplateforme.io>
2022-11-16 10:26:00 +0100

    rendu job01 affichage des nombre paire et impaire du tableau

---
+++ jour03/job01/index.php
@@ -0,0 +1,12 @@
+<?php
+$valeurs = [200, 204, 173, 98, 171, 404, 459];
+
+foreach ($valeurs as $nombre)
+    if ($nombre%2 === 0){
+        echo
+            "$nombre est paire<br/>";
+    }
+    else {
+        echo "$nombre est impaire<br/>";
+    }
+?>
